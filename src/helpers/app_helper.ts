de89c369f8d6828ff6098de5ea732a982722df18
Danielle Jackson <djackson@dialexa.com>
2023-08-21 10:18:19 -0700

    README

--- src/helpers/app_helper.ts
+++ src/helpers/app_helper.ts
@@ -1,10 +1,12 @@
 import express, { Express, json, urlencoded } from 'express';
 import { RegisterRoutes } from "../generated/routes";
 import { ProvideSingleton } from "../ioc";
+import { info } from "./";
 
 @ProvideSingleton(AppHelper)
 export class AppHelper {
 	private app: Express | undefined;
+	private listenPort = process.env.PORT ?? 3333;
 
 	public start() {
 		if (!this.app) {
@@ -13,7 +15,8 @@ export class AppHelper {
 				extended: true,
 			}));
 			this.app.use(json());
-			this.app.listen(process.env.PORT ?? 3333, () => {
+			this.app.listen(this.listenPort, () => {
+			    info(`Application listening on ${this.listenPort}`);
 			});
 			RegisterRoutes(this.app);
 		}
